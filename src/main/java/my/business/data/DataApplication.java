c2a566a6aa7142df261bf80a42ed54059e7fd669
gnn <gnn@gmail.com>
2020-07-16 11:48:24 +0300

    [F-1] : a bit refactor

--- src/main/java/my/business/data/DataApplication.java
+++ src/main/java/my/business/data/DataApplication.java
@@ -1,11 +1,13 @@
 package my.business.data;
 
+import my.business.data.entities.Deal;
 import my.business.data.logic.StringReader;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class DataApplication {
@@ -16,9 +18,14 @@ public class DataApplication {
 		File file = new File("C:\\Users\\nick\\Desktop\\res_" + LocalDate.now().getDayOfMonth() + "_" +
 				LocalDate.now().getMonth() + "_" +
 				LocalDate.now().getYear() + ".txt");
+		String absolutePath = file.getAbsolutePath();
 
 		StringReader stringReader = new StringReader();
+		ArrayList<Deal> deals = stringReader.readFile(args[0]);
 
-		stringReader.writeJsonToFile(file.getAbsolutePath(), stringReader.readFile(args[0]));
+		Deal deal = deals.get(37623);
+		Deal deal1 = deals.get(37624);
+
+		stringReader.writeJsonToFile(absolutePath, deals);
 	}
 }
