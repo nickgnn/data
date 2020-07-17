18ed10b525b7333a45df65ac3991a367f876c2d8
gnn <gnn@gmail.com>
2020-07-17 15:36:41 +0300

    [F-2] : write controller

--- src/main/java/my/business/data/controller/MainController.java
+++ src/main/java/my/business/data/controller/MainController.java
@@ -1,7 +1,29 @@
 package my.business.data.controller;
 
-@Controller
-public class Controller {
-    @GetMapping("/")
+import com.google.gson.internal.LinkedTreeMap;
+import my.business.data.logic.StringReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+@RestController
+public class MainController {
+    private StringReader stringReader;
+
+    @Autowired
+    public MainController(StringReader stringReader) {
+        this.stringReader = stringReader;
+    }
+
+    @RequestMapping("/")
+    public String index() throws IOException {
+        ArrayList<LinkedTreeMap> deals = stringReader.readFile("C:\\Users\\nick\\Desktop\\res_15_JULY_2020.txt");
+
+        stringReader.writeDealsToDB(deals);
+
+        return "All Done!";
+    }
 }
